d5c2c2df0a2d3e9345215454956d458367dd18b8
DarienSngoeun <darien.sngoeun@gmail.com>
2025-09-18 19:48:43 -0700

    fixing server

--- application/backend/server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.get('/', (_,res)=>res.send('Hello from my repo!'));
-const port = process.env.PORT || 3000;
-app.listen(port, '127.0.0.1', () => console.log('Listening on', port));
