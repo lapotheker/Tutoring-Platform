e296bf7afd3f5fc8b666ee43441175a010b3858c
DarienSngoeun <darien.sngoeun@gmail.com>
2025-09-18 21:30:01 -0700

    fixing pm2

--- application/backend/ecosystem.config.js
+++ application/backend/ecosystem.config.js
@@ -17,11 +17,10 @@ module.exports = {
       repo: 'git@github.com:CSC-648-SFSU/csc648-fa25-145-team04.git',
       path: '/home/ubuntu/apps/csc648',     // base deploy dir (PM2 manages current/)
       'post-deploy': [
-        // 1) run npm in the backend folder
         'cd application/backend',
         'npm ci',
-        // 2) start/restart using the config that lives in application/backend
-        'pm2 startOrRestart /home/ubuntu/apps/csc648/current/application/backend/ecosystem.config.js --only backend',
+        // relative to current/application/backend
+        'pm2 startOrRestart ecosystem.config.js --only backend',
         'pm2 save'
       ].join(' && ')
     }
