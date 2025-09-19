ef832a53d00f24d949f11da017f29be8ad9a53ea
DarienSngoeun <darien.sngoeun@gmail.com>
2025-09-18 23:36:40 -0700

    fixing static page on remote server

--- application/backend/index.js
+++ application/backend/index.js
@@ -1,12 +1,4 @@
-// application/backend/index.js
-const path = require('path');
-const express = require('express');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
-});
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+app.listen(3000, () => console.log('Server running on port 3000'))
