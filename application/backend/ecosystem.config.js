51b493062e97bad5eb8eb7b94860ce958c842a35
DarienSngoeun <darien.sngoeun@gmail.com>
2025-09-20 16:48:15 -0700

    fixing pm2

--- application/backend/ecosystem.config.js
+++ application/backend/ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [{
     name: 'backend',
     cwd: "application/backend",
-    script: './index.js'
+    script: './index.js',
+    env: { NODE_ENV: "production", PORT: "3000" }
   }],
   deploy: {
     production: {
@@ -12,7 +13,16 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@github.com:CSC-648-SFSU/csc648-fa25-145-team04.git',
       path: '/home/ubuntu/csc648-fa25-145-team04',
-      "post-deploy": "npm ci --omit=dev --prefix application/backend && pm2 startOrRestart application/backend/ecosystem.config.js --only backend"
+      "post-deploy": 
+        // install backend deps
+        "npm ci --omit=dev --prefix application/backend && " +
+        // build the Vite app
+        "npm ci --prefix application/team-info-website && " +
+        "npm run --prefix application/team-info-website build && " +
+        // publish static files to Nginx root
+        "rsync -ah --delete application/team-info-website/dist/ /var/www/team04-frontend/ && " +
+        // restart api
+        "pm2 startOrRestart ecosystem.config.js --only backend && pm2 save"
     }
   }
 }
