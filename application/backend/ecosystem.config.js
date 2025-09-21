5e39785b4a058409d79ae00c8d700a064e5cc514
DarienSngoeun <darien.sngoeun@gmail.com>
2025-09-21 11:19:13 -0700

    Update deploy host + build frontend on deploy

--- application/backend/ecosystem.config.js
+++ application/backend/ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [{
     name: 'backend',
     cwd: "application/backend",
-    script: './index.js',
+    script: 'index.js',
     env: { NODE_ENV: "production", PORT: "3000" }
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-54-219-99-133.us-west-1.compute.amazonaws.com',
-      key: '~/.ssh/team04-project.pem',
+      host: 'ec2-54-193-205-119.us-west-1.compute.amazonaws.com',
+      key: 'credentials/team04-project.pem',
       ref: 'origin/main',
       repo: 'git@github.com:CSC-648-SFSU/csc648-fa25-145-team04.git',
       path: '/home/ubuntu/csc648-fa25-145-team04',
