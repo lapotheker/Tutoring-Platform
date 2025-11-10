db98baffaed6677ddcea61f4481126ada6aa9363
Yuhang Wei <yuhangwei@MacBook-Air.local>
2025-11-09 20:24:13 -0800

    Updated DB

--- application/backend/db/pool.js
+++ application/backend/db/pool.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "50.18.23.168",
   user: "class_cto",
   password: "CTO_TEMP_PASSWORD",
-  database: "teamapp_db",
+  database: "main_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
